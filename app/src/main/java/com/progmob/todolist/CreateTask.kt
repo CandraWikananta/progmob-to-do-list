1a815f7122d1198ca81d2b73322362d04ae192ca
Candra Wikananta <candrawikananta@gmail.com>
2025-06-25 22:35:27 +0800

    membuat tampilan landing page, create task, dan splash screen

---
+++ app/src/main/java/com/progmob/todolist/CreateTask.kt
@@ -0,0 +1,35 @@
+package com.progmob.todolist
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CreateTask : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_create_task)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Di dalam onCreate()
+        val categorySpinner: Spinner = findViewById(R.id.spinnerCategory)
+        val prioritySpinner: Spinner = findViewById(R.id.spinnerPriority)
+
+// Data Spinner
+        val categories = arrayOf("Personal", "Kuliah", "Kerja")
+        val priorities = arrayOf("Low", "Medium", "High")
+
+// Adapter Category
+        val categoryAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, categories)
+        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        categorySpinner.adapter = categoryAdapter
+    }
+}
