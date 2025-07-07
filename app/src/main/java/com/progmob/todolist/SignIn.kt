15c22f5bf16678e011883ac7a4e4539305ad8853
SeptiNo <madeseptinobudiputrawan090@gmail.com>
2025-07-07 16:24:43 +0800

    add logic from splash screen, welcome page, sign in and up

---
+++ app/src/main/java/com/progmob/todolist/SignIn.kt
@@ -0,0 +1,19 @@
+package com.progmob.todolist
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_in_form_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signInForm)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
