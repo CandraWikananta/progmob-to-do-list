45c433c4954291c0c1fd4c7978a3d32120697818
Candra Wikananta <candrawikananta@gmail.com>
2025-07-09 00:50:23 +0800

    add: tombol plus mengarahkan ke create task view, tombol back pada create task view mengarahkan kembali ke landing page

--- app/src/main/java/com/progmob/todolist/CreateTaskActivity.kt
+++ app/src/main/java/com/progmob/todolist/CreateTaskActivity.kt
@@ -1,5 +1,6 @@
 package com.progmob.todolist
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Spinner
@@ -7,12 +8,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.progmob.todolist.databinding.ActivityCreateTaskBinding
 
 class CreateTaskActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCreateTaskBinding
+    private lateinit var databaseHelper: DatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_create_task)
+        binding = ActivityCreateTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        databaseHelper = DatabaseHelper(this)
+
+        // tombol back
+        binding.backButton.setOnClickListener{
+            val intent = Intent(this, LandingPageActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/progmob/todolist/LandingPageActivity.kt
+++ app/src/main/java/com/progmob/todolist/LandingPageActivity.kt
@@ -1,16 +1,32 @@
 package com.progmob.todolist
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.progmob.todolist.databinding.ActivityLandingPageBinding
 
 class LandingPageActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLandingPageBinding
+    private lateinit var databaseHelper: DatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_landing_page)
+        binding = ActivityLandingPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        databaseHelper = DatabaseHelper(this)
+
+        binding.addTask.setOnClickListener{
+            val intent = Intent(this, CreateTaskActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
